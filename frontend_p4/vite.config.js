9c10927839c92886515059251be5e2fe311c38a7
Juan Larrondo <juanlarrondofdez@gmail.com>
2026-04-25 11:17:37 +0200

    url backend render

--- frontend_p4/vite.config.js
+++ frontend_p4/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'https://three-songproject-99-2311-2026-v1.onrender.com',
         changeOrigin: true
       }
     }
