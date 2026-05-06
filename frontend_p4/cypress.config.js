1c431db636d07f0d8c4e3ea72fa4440690e1afe7
Juan Larrondo <juanlarrondofdez@gmail.com>
2026-05-06 13:50:20 +0200

    prueba render 3

--- frontend_p4/cypress.config.js
+++ frontend_p4/cypress.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
-    //baseUrl: 'https://song-frontend-shl2xxxxxx.onrender.com',
+    //baseUrl: 'http://localhost:4173',
+    baseUrl: 'https://four-songproject-99-2311-2026-v1.onrender.com',
   },
   component: {
     specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
